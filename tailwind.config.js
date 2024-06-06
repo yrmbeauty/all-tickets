51b2be558e8605ae8f4e29a7364619376e3ee178
yrmbeauty <maxim.konovodov@gmail.com>
2024-06-06 12:06:21 +0400

    delete unused

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
