efcec14070d66ee71d76964cb050261277addd3a
yrmbeauty <maxim.konovodov@gmail.com>
2024-06-06 17:26:35 +0400

    pretify

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -16,7 +16,7 @@ export const Ticket: React.FC<Props> = props => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
+        <p className="line-clamp-3">{description}</p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary" onClick={onClick}>
             {status === TicketStatus.activated ? 'Show' : 'Activate'}
--- src/pages/TicketPage.tsx
+++ src/pages/TicketPage.tsx
@@ -6,15 +6,17 @@ const TicketPage: React.FC<Props> = props => {
   const { name, description, status } = props;
 
   return (
-    <div className="flex-1">
+    <div className="flex flex-1 flex-col overflow-scroll">
       <figure>
         <img
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
           alt="Shoes"
         />
       </figure>
-      <h1 className="text-lg">{name}</h1>
-      <p className="text-base">{description}</p>
+      <div className="p-6 overflow-auto">
+        <h1 className="text-5xl font-bold">{name}</h1>
+        <p className="text-base">{description}</p>
+      </div>
     </div>
   );
 };
