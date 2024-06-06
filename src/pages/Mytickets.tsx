f76a22725deaf106c97b2c64eb6e9187f5de72e7
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2024-06-06 13:51:31 +0400

    styles

--- src/pages/Mytickets.tsx
+++ src/pages/Mytickets.tsx
@@ -1,27 +1,35 @@
-import { Ticket } from "../components/Ticket";
+import { Ticket } from '../components/Ticket'
 
 type Props = {}
 
 const ticket = {
-  name: "Ticket",
-  description: "Concert description",
+  name: 'Ticket',
+  description: 'Concert description',
   isActive: true,
 }
 
-const activeTickets = new Array(2).fill(ticket);
-const tickets = new Array(2).fill({...ticket, isActive: false});
+const activeTickets = new Array(2).fill(ticket)
+const tickets = new Array(2).fill({ ...ticket, isActive: false })
 
-const Mytickets: React.FC<Props> = props => {
-  const {} = props;
+const Mytickets: React.FC<Props> = (props) => {
+  const {} = props
 
   return (
     <div>
-      <h1 className="text-lg">Active</h1>
-      <div>{activeTickets.map((ticket) => <Ticket {...ticket} />)}</div>
-      <h1 className="text-lg">List</h1>
-      <div>{tickets.map((ticket) => <Ticket {...ticket} />)}</div>
+      <h1 className='text-lg'>Active</h1>
+      <div className='flex gap-4 flex-wrap'>
+        {activeTickets.map((ticket) => (
+          <Ticket {...ticket} />
+        ))}
+      </div>
+      <h1 className='text-lg'>List</h1>
+      <div className='flex gap-4 flex-wrap'>
+        {tickets.map((ticket) => (
+          <Ticket {...ticket} />
+        ))}
+      </div>
     </div>
   )
-};
+}
 
-export default Mytickets;
+export default Mytickets
