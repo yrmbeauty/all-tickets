ad83d519eebe8ac9f05125a7d57269b9659487e0
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2024-06-06 14:04:09 +0400

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import Mytickets from "./pages/Mytickets";
-import "./App.css";
-// import { TonConnectButton } from "@tonconnect/ui-react";
-// import { useTonConnect } from "./hooks/useTonConnect";
-// import { CHAIN } from "@tonconnect/protocol";
-import "@twa-dev/sdk";
+import '@twa-dev/sdk';
+import Mytickets from './pages/Mytickets';
+import './App.css';
+import { CHAIN, TonConnectButton } from '@tonconnect/ui-react';
+import { useTonConnect } from './hooks/useTonConnect';
 
 function App() {
-  // const { network } = useTonConnect();;
+  const { network } = useTonConnect();
 
   return (
-    <div>
+    <>
+      <header className="flex gap-4 items-center">
+        <TonConnectButton className="ml-auto" />
+        {network
+          ? // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+            network === CHAIN.MAINNET
+            ? 'mainnet'
+            : 'testnet'
+          : 'N/A'}{' '}
+      </header>
+
       <Mytickets />
-    </div>
+
+      <footer className="">
+        <button className="btn"></button>
+      </footer>
+    </>
   );
 }
 
 export default App;
-
-// <TonConnectButton />
-// {network
-//   ? network === CHAIN.MAINNET
-//     ? "mainnet"
-//     : "testnet"
-//   : "N/A"}
--- src/pages/Mytickets.tsx
+++ src/pages/Mytickets.tsx
@@ -1,35 +1,40 @@
-import { Ticket } from '../components/Ticket'
+import { Ticket } from '../components/Ticket';
 
-type Props = {}
+type Props = {};
 
 const ticket: Entity.Ticket = {
   name: 'Ticket',
   description: 'Concert description',
-  status: Entity.TicketStatus.activated,
-}
+  status: Entity.TicketStatus.activated
+};
 
-const activeTickets = new Array(2).fill(ticket)
-const tickets = new Array(2).fill({ ...ticket, isActive: false })
+const activeTickets = new Array(2).fill(ticket);
+const tickets = new Array(2).fill({ ...ticket, isActive: false });
 
 const Mytickets: React.FC<Props> = (props) => {
-  const {} = props
+  const {} = props;
 
   return (
-    <div>
-      <h1 className='text-lg'>Active</h1>
-      <div className='flex gap-4 flex-wrap'>
-        {activeTickets.map((ticket) => (
-          <Ticket {...ticket} />
-        ))}
+    <div className="flex flex-col gap-4 flex-1 overflow-auto">
+      <div>
+        <h1 className="text-lg">Active</h1>
+        <div className="flex gap-4 flex-wrap">
+          {activeTickets.map((ticket) => (
+            <Ticket {...ticket} />
+          ))}
+        </div>
       </div>
-      <h1 className='text-lg'>List</h1>
-      <div className='flex gap-4 flex-wrap'>
-        {tickets.map((ticket) => (
-          <Ticket {...ticket} />
-        ))}
+
+      <div>
+        <h1 className="text-lg">List</h1>
+        <div className="flex gap-4 flex-wrap">
+          {tickets.map((ticket) => (
+            <Ticket {...ticket} />
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mytickets
+export default Mytickets;
